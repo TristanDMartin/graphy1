bd6f1bd498da4781bd7a1e19dd4a1ffc38b1f16f
Lucas Nunes <nunesmlucasg@gmail.com>
2019-03-18 13:34:52 -0400

    error catch for <5 articles

--- public/js/nytCode.js
+++ public/js/nytCode.js
@@ -31,65 +31,65 @@ function updatePage(result) {
   for (var i = 0; i < 5; i++) {
     // Get specific article info for current index
     var article = result.articles[i];
-    console.log(article);
+    if (article) {
+      console.log(article);
 
-    // Increase the articleCount (track article # - starting at 1)
-    var articleCount = i + 1;
+      // Increase the articleCount (track article # - starting at 1)
+      var articleCount = i + 1;
 
-    // Create the  list group to contain the articles and add the article content for each
-    var $articleList = $("<ul>");
-    $articleList.addClass("list-group");
+      // Create the  list group to contain the articles and add the article content for each
+      var $articleList = $("<ul>");
+      $articleList.addClass("list-group");
 
-    // Add the newly created element to the DOM
-    $("#article-section").append($articleList);
+      // Add the newly created element to the DOM
+      $("#article-section").append($articleList);
 
-    // If the article has a headline, log and append to $articleList
-    var headline = article.title;
-    console.log("HEADLINE ========= : " + headline);
-    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+      // If the article has a headline, log and append to $articleList
+      var headline = article.title;
+      console.log("HEADLINE ========= : " + headline);
+      var $articleListItem = $("<li class='list-group-item articleHeadline'>");
 
-    if (headline) {
-      console.log(headline);
-      $articleListItem.append(
-        "<span class='label label-primary'>" +
-        articleCount +
-        "</span>" +
-        "<strong> " +
-        headline +
-        "</strong>"
-      );
+      if (headline) {
+        console.log(headline);
+        $articleListItem.append(
+          "<span class='label label-primary'>" +
+          articleCount +
+          "</span>" +
+          "<strong> " +
+          headline +
+          "</strong>"
+        );
+      }
+      else {
+        headline = "No Title";
+      }
+
+      // Log section, and append to document if exists
+      var description = article.description;
+      console.log(article.description);
+      if (description) {
+        $articleListItem.append("<h5>Description: " + description + "</h5>");
+      }
+
+      // Log published date, and append to document if exists
+      var publishedDate = article.publishedAt;
+      console.log(article.publishedAt);
+
+      if (publishedDate) {
+        publishedDate = moment(publishedDate).calendar();
+        console.log("NEW MOMENT DATE/TIME: " + publishedDate);
+        $articleListItem.append("<h5>" + publishedDate + "</h5>");
+      }
+      else {
+        publishedDate = "Cannot find published date.";
+      }
+      // Append and log url
+      $articleListItem.append("<a href='" + article.url + "'>URL Here!</a>");
+      console.log(article.url);
+
+      // Append the article
+      $articleList.append($articleListItem);
     }
-    else {
-      headline = "No Title";
-    }
-
-    // Log section, and append to document if exists
-    var description = article.description;
-    console.log(article.description);
-    if (description) {
-      $articleListItem.append("<h5>Description: " + description + "</h5>");
-    }
-
-    // Log published date, and append to document if exists
-    var publishedDate = article.publishedAt;
-    console.log(article.publishedAt);
-
-    if (publishedDate) {
-      publishedDate = moment(publishedDate).calendar();
-      console.log("NEW MOMENT DATE/TIME: " + publishedDate);
-      $articleListItem.append("<h5>" + publishedDate + "</h5>");
-    }
-    else{
-      publishedDate = "Cannot find published date.";
-    }
-
-
-    // Append and log url
-    $articleListItem.append("<a href='" + article.url + "'>URL Here!</a>");
-    console.log(article.url);
-
-    // Append the article
-    $articleList.append($articleListItem);
   }
 }
 // =====================================================================================================
